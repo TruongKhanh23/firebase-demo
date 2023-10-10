105520f7976410a3984d46d789b27a5d016458e7
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-10-10 14:01:51 +0700

    feat: set up vue router

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'; // Import the necessary functions and objects
+
+import HomePage from './../components/HomePage.vue';
+import Login from './../components/Login.vue';
+import Register from './../components/Register.vue';
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
+];
+
+const router = createRouter({
+  history: createWebHistory(), // Use createWebHistory for the default history mode
+  routes,
+});
+
+export default router;
