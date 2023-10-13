773ba30a98146e0905abaf64e24ff385b5313406
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-10-13 13:53:36 +0700

    feat: login, register, login With Google, sign out, check access

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -22,6 +22,7 @@
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             placeholder="name@company.com"
             required
+            v-model="email"
           />
         </div>
         <div>
@@ -36,9 +37,10 @@
             id="password"
             placeholder="••••••••"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            required
+            v-model="password"
           />
         </div>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
         <div class="flex items-start">
           <div class="flex items-start">
             <div class="flex items-center h-5">
@@ -47,7 +49,6 @@
                 type="checkbox"
                 value=""
                 class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                required
               />
             </div>
             <label
@@ -65,6 +66,7 @@
         <button
           type="submit"
           class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          @click="login"
         >
           Login to your account
         </button>
@@ -79,10 +81,47 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "Login",
+<script setup>
+import { ref } from "vue";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "vue-router"
+
+const email = ref("");
+const password = ref("");
+const errorMessage = ref()
+const router = useRouter()
+
+const login = () => {
+  console.log("went register");
+  const auth = getAuth()
+  signInWithEmailAndPassword(auth, email.value, password.value).then(
+    (data) => {
+      console.log("Successfully signed in!");
+
+      console.log(auth.currentUser)
+      // router.push("/")
+    },
+  ).catch((error) => {
+    console.log(error.code);
+    console.log("went catch");
+    switch(error.code) {
+      case "auth/invalid-email":
+        errorMessage.value = "Invalid Email"
+        break
+      case "auth/user-not-founđ":
+        errorMessage.value = "No account with that email was found"
+        break
+      case "auth/wrong-password":
+        errorMessage.value = "Incorrect password"
+        break
+      default: 
+        errorMessage.value = "Email or password was incorrect"
+        break
+    }
+  })
 };
+
+const signInWithGoogle = () => {};
 </script>
 
 <style scoped></style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -21,6 +21,7 @@
             id="email"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             placeholder="name@company.com"
+            v-model="email"
             required
           />
         </div>
@@ -35,6 +36,7 @@
             name="password"
             id="password"
             placeholder="••••••••"
+            v-model="password"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             required
           />
@@ -42,8 +44,16 @@
         <button
           type="submit"
           class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          @click="register"
         >
           Submit
+        </button>
+        <button
+          type="submit"
+          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          @click="signInWithGoogle"
+        >
+          Sign in with Google
         </button>
         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
           Have account already?
@@ -58,9 +68,46 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "Register",
+<script setup>
+import { ref } from "vue";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { useRouter } from "vue-router";
+
+const email = ref("");
+const password = ref("");
+const router = useRouter();
+
+const register = () => {
+  console.log("went register");
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((data) => {
+      console.log("Successfully registered!");
+
+      console.log(auth.currentUser);
+      // router.push("/")
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
+};
+
+const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      console.log(result.user);
+      router.push("/");
+    })
+    .catch((error) => {
+      //handle error
+    });
 };
 </script>
 
