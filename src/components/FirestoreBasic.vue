98825d9a1327824dec893cbeb3cf8a5d6a27870c
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2023-10-21 22:54:18 +0700

    feat: add column to a document, or update new value to a column

--- src/components/FirestoreBasic.vue
+++ src/components/FirestoreBasic.vue
@@ -15,6 +15,13 @@
     >
       Create Country
     </button>
+
+    <button
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      @click="addColumnOrUpdateValueToColumn"
+    >
+      Add capital column to country
+    </button>
   </div>
 </template>
 <script setup>
@@ -42,6 +49,19 @@ const updateOrCreateCountry = async () => {
     alert(`Set country ${dataObject.name} successfully`);
   } catch (error) {
     alert("Failed to set country");
+  }
+};
+
+const addColumnOrUpdateValueToColumn = async () => {
+  const dataObject = { capital: "Ha Noi" };
+  const documentId = "VN";
+  try {
+    await setDoc(doc(db, "countries", documentId), dataObject, { merge: true });
+    alert(
+      `Add column capital with value: ${dataObject.capital} to document ${documentId} successfully`,
+    );
+  } catch (error) {
+    alert("Failed to add column");
   }
 };
 </script>
