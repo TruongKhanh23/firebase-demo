a66409de5451a316cbb639830dba4fc4ea6ca643
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2023-10-21 21:08:51 +0700

    feat: add firestore basic to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -51,6 +51,13 @@
           </li>
           <li>
             <a
+              href="/firestore-basic"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Firestore Basic</a
+            >
+          </li>
+          <li>
+            <a
               href="/login"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >Login</a
@@ -79,28 +86,28 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import router from "@/router";
 
-const isLoggedIn = ref(false)
+const isLoggedIn = ref(false);
 
-let auth
+let auth;
 onMounted(() => {
-  auth = getAuth()
+  auth = getAuth();
   onAuthStateChanged(auth, (user) => {
-    if(user) {
-      isLoggedIn.value = true
+    if (user) {
+      isLoggedIn.value = true;
     } else {
-      isLoggedIn.value = false
+      isLoggedIn.value = false;
     }
-  })
-})
+  });
+});
 
 const handleSignOut = () => {
   signOut(auth).then(() => {
-    router.push("/login")
-  })
-}
+    router.push("/login");
+  });
+};
 </script>
 
 <style></style>
