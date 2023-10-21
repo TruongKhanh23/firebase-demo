81fa12a97b7611cac38bc67ca6520a54179f1d7d
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2023-10-21 21:26:10 +0700

    feat: init firebase app, export db

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,9 +15,10 @@ const firebaseConfig = {
   projectId: "kdotkhanh-firebase-demo",
   storageBucket: "kdotkhanh-firebase-demo.appspot.com",
   messagingSenderId: "519468087445",
-  appId: "1:519468087445:web:c61563afed34bf6109c39c"
+  appId: "1:519468087445:web:c61563afed34bf6109c39c",
 };
 
-initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp);
 
 createApp(App).use(router).mount("#app");
