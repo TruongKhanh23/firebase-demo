3d122d077079f4861a1c97621e97f7fc95657204
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2023-10-22 15:14:24 +0700

    feat: getDoc (select 1 row) by getCountry

--- src/components/FirestoreBasic.vue
+++ src/components/FirestoreBasic.vue
@@ -22,12 +22,20 @@
     >
       Add capital column to country
     </button>
+
+    <div>Capital: {{ capital }}</div>
+    <button
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      @click="getCountry"
+    >
+      Get country information
+    </button>
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
 import { db } from "../main";
-import { collection, addDoc, doc, setDoc } from "firebase/firestore";
+import { collection, addDoc, doc, setDoc, getDoc } from "firebase/firestore";
 
 const createUser = async () => {
   const collRef = collection(db, "users");
@@ -62,6 +70,20 @@ const addColumnOrUpdateValueToColumn = async () => {
     );
   } catch (error) {
     alert("Failed to add column");
+  }
+};
+
+const capital = ref("");
+const getCountry = async () => {
+  // attempt to retrieve the country
+  const documentSnapshot = await getDoc(doc(db, "countries", "VN"));
+
+  // if it exists, alert it
+  if (documentSnapshot.exists()) {
+    alert(JSON.stringify(documentSnapshot.data()));
+    capital.value = documentSnapshot.data().capital;
+  } else {
+    alert("Document does not exist");
   }
 };
 </script>
